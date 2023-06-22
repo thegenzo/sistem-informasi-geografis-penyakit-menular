d88e6baabc1402d1729f3ad53754c5ec92336c4d
Dicky Agusputra <loscarbulos@gmail.com>
2023-06-22 20:19:22 +0800

    Update dashboard.blade.php

--- resources/views/admin-panel/pages/dashboard.blade.php
+++ resources/views/admin-panel/pages/dashboard.blade.php
@@ -9,53 +9,6 @@
             <div class="section-header">
                 <h1>Home</h1>
             </div>
-            {{-- <div class="row">
-                <div class="col-lg-4 col-md-12 col-sm-12 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-primary">
-                            <i class="far fa-user"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Nama Situs</h4>
-                            </div>
-                            <div class="card-body">
-                                {{ $site_config->site_name ?? '-' }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-12 col-sm-12 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
-                            <i class="far fa-newspaper"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Total Berita</h4>
-                            </div>
-                            <div class="card-body">
-                                {{ $newsCount }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-12 col-sm-12 col-12">
-                    <div class="card card-statistic-1">
-                        <div class="card-icon bg-danger">
-                            <i class="far fa-newspaper"></i>
-                        </div>
-                        <div class="card-wrap">
-                            <div class="card-header">
-                                <h4>Total Dokumen</h4>
-                            </div>
-                            <div class="card-body">
-                                {{ $documentCount }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
             <div class="row">
                 <div class="col-12 mb-4">
                     <div class="hero bg-primary text-white">
@@ -64,6 +17,53 @@
                             {{-- <p class="lead">Silahkan pilih menu di sebelah kiri @ceklevel('admin')
                                     atau dibawah
                                 @endceklevel untuk mengatur situs</p> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-primary">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Total Data Kecamatan</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ \App\Models\District::count() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="far fa-newspaper"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Total Data FASKES</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ \App\Models\HealthcareFacilities::count() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="far fa-newspaper"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Total Data Penyakit</h4>
+                            </div>
+                            <div class="card-body">
+                                {{ \App\Models\Disease::count() }}
+                            </div>
                         </div>
                     </div>
                 </div>
