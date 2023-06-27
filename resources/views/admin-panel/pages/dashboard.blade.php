c3d23de6554e04126c2106589652f841b3bd0e2b
Dicky Agusputra <loscarbulos@gmail.com>
2023-06-27 22:39:03 +0800

    Update dashboard.blade.php

--- resources/views/admin-panel/pages/dashboard.blade.php
+++ resources/views/admin-panel/pages/dashboard.blade.php
@@ -83,7 +83,14 @@
             </div>
             <div class="row">
                 <div class="col-xl-12 col-lg-12 col-md-12">
-                    <div id="map"></div>
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Data Persebaran Penyakit Menular Kota Baubau</h4>
+                        </div>
+                        <div class="card-body">
+                            <div id="map"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
