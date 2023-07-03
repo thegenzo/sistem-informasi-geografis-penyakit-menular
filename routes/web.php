5f638b40e7d4872a8de6dae8ad95c568fa987dc1
Dicky Agusputra <loscarbulos@gmail.com>
2023-07-03 19:39:20 +0800

    401 unauthorized on homepage

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,6 @@ Route::middleware('auth')->group(function () {
 
     Route::group(['prefix' => 'admin-panel'], function () {
         Route::get('/dashboard', [AdminPanelController::class, 'index'])->name('admin-panel.dashboard');
-        Route::get('/get-all-districts', [AdminPanelController::class, 'getAllDistricts']);
-        Route::get('/get-all-healthcares', [AdminPanelController::class, 'getAllHealthcares']);
 
         Route::resource('diseases', DiseaseController::class, ['as' => 'admin-panel']);
 
@@ -48,6 +46,11 @@ Route::middleware('auth')->group(function () {
 
         Route::resource('cases', CasesController::class, ['as' => 'admin-panel']);
     });
+});
+
+Route::group(['prefix' => 'admin-panel'], function () {
+    Route::get('/get-all-districts', [AdminPanelController::class, 'getAllDistricts']);
+    Route::get('/get-all-healthcares', [AdminPanelController::class, 'getAllHealthcares']);
 });
 
 require __DIR__.'/auth.php';
